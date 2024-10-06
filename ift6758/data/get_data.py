9dbbfc011a2e70a37e28e0f9027a2a37c97231e1
Andre Diler <andre.diler@polymtl.ca>
2024-10-06 12:39:27 -0400

    add function to download playoff data and display it in debugger

--- ift6758/data/get_data.py
+++ ift6758/data/get_data.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 from pathlib import Path 
+from json.decoder import JSONDecodeError
 
 # https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md
 # https://en.wikipedia.org/wiki/List_of_NHL_seasons
@@ -25,26 +26,76 @@ game_number_per_year = {
     "2023": "1353" # 2023-2024
     }
 
-def retrieve_game_data(game_id) -> dict:
-    
-    data_dir = Path(__file__).resolve().parent.as_posix() + "/raw_data" # path to the raw data directory relative to this file
-    filename = f"{game_id}.json"
+# will generate all playoff games, but some might not work
+# ex: if team wins 4 games in a row -> will advance to next game
+def playoff_game_id_generator(season) -> list[str]:
+    game_ids = []
+    for round in range(1, 4+1):
+        if round == 1:
+            game_ids += matchups(season, round, 8) # 8 games in round 1 for 16 teams
+        elif round == 2:
+            game_ids += matchups(season, round, 4) # 4 games in round 2 for 8 teams
+        elif round == 3:
+            game_ids += matchups(season, round, 2) # 2 games in round 3 for 4 teams
+        elif round == 4:
+            game_ids += matchups(season, round, 1) # 1 games in round 4 for 2 teams
+    return game_ids
 
-    if os.path.exists(f"{data_dir}/{filename}"):
-        with open(f"{data_dir}/{filename}", 'r') as file:
-            print("using cached data")
-            return json.load(file)
+def matchups(season, round, matchup_range):
+    game_ids = []
+    for matchup in range(1, matchup_range +1):
+        for best_of_seven in range(1, 7+1):
+            game_ids.append(f"{season}_{PLAYOFFS}_0-{round}-{matchup}-{best_of_seven}")
+    return game_ids
 
+def clean_playoff_game_id(game_id) -> str:
+    return game_id.replace('_', '').replace('-', '')
+
+
+def regular_season_game_id_generator(season) -> list[str]:
+    game_ids = []
+    for game_number in range(1, int(game_number_per_year[season]) + 1):
+        game_id = f"{season}{REGULAR_SEASON}{game_number:04d}"
+        game_ids.append(game_id)
+    return game_ids
+
+def download_data(game_id) -> dict:
     response = requests.get(f"https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play")
 
     if response.status_code == 200:
         data = response.json()
-        
-        with open(f"{data_dir}/{filename}", 'w') as file:
-            json.dump(data, file)
-        
+        print("Downloaded game id: ", game_id)     
         return data
     else:
-        print(f"Failed to retrieve data: {response.status_code}")
+        print(f"Failed to retrieve game ID {game_id}: {response.status_code}")
         return None
+
+
+def load_cached_data(file_path) -> dict:
+     with open(file_path, 'r') as file:
+            print("using cached data for game id: ", file_path.split('/')[-1])  
+            try:
+                data = json.load(file)
+                return data
+            except JSONDecodeError:
+                print(f"Error decoding JSON from response for game ID: {file_path.split('/')[-1]}")
+            return None
+     
+def retrieve_game_data(game_id) -> dict:
+    data_dir = Path(__file__).resolve().parent.as_posix() + "/raw_data" # path to the raw data directory relative to this file
+    filename = f"{game_id}.json"
+    file_path = f"{data_dir}/{filename}"
     
+    if os.path.exists(file_path):
+        data = load_cached_data(file_path)
+        if data:
+            return data
+        else: # file is present but corrupted: remove it, and redownload
+            os.remove(file_path)
+  
+    data = download_data(game_id)
+    if data: # do not create a file if the download failed    
+        with open(f"{data_dir}/{filename}", 'w') as file:
+            json.dump(data, file)
+    
+    return data
--- ift6758/visualizations/debugging_tool.py
+++ ift6758/visualizations/debugging_tool.py
@@ -4,7 +4,7 @@ from IPython.display import display, clear_output
 import matplotlib.pyplot as plt
 import numpy as np
 import ipywidgets as widgets
-from ift6758.data import retrieve_game_data, game_number_per_year, REGULAR_SEASON
+from ift6758.data import get_data
 
 # Load the image
 rink_image_path = '../figures/nhl_rink.png'
@@ -72,24 +72,38 @@ def plot_coords_on_image(rink_coord_x, rink_coord_y, image=rink_image, debug_ori
 
     plt.show()
 
-def display_season_data(season: str) -> None:
-    game_number = int(game_number_per_year[season])
-    game_id_slider = widgets.IntSlider(min=1, max=game_number, step=1, description='Game ID:')
-    display(game_id_slider)
 
-    def print_game_info(game_data):
-        # TODO: display the sides of the team depending on the period (problem: HomeTeamDefendingSide available from 2019 only)
-        print("id" , game_data["id"])
-        print("date" , game_data["gameDate"])
-        print("Venue Location: ", game_data["venueLocation"]["default"])
-        print("Home Team: ", game_data["homeTeam"]["name"]["default"])
-        print("Away Team:", game_data["awayTeam"]["name"]["default"])
+def print_game_info(game_data):
+    # TODO: display the sides of the team depending on the period (problem: HomeTeamDefendingSide available from 2019 only)
+    print("id" , game_data["id"])
+    print("date" , game_data["gameDate"])
+    print("Venue Location: ", game_data["venueLocation"]["default"])
+    print("Home Team: ", game_data["homeTeam"]["name"]["default"])
+    print("Away Team:", game_data["awayTeam"]["name"]["default"])
+    print()
 
+def print_event_info(event):
+    print("event id: ", event["eventId"])
+    print("event description: ", event["typeDescKey"])
+    if "details" in event:
+        if "xCoord" in event["details"] and "yCoord" in event["details"]:
+            print("x: ", event["details"]["xCoord"], "y: ", event["details"]["yCoord"])
+            plot_coords_on_image(event["details"]["xCoord"], event["details"]["yCoord"], debug_origin=False)
+        else:
+            print("No coordinates available")
 
+def display_regular_season(season: str):
     def on_game_id_change(change):
-        game_id = f"{season}{REGULAR_SEASON}{change['new']:04d}"
-        data = retrieve_game_data(game_id)
+        game_id = f"{season}{get_data.REGULAR_SEASON}{change['new']:04d}"
+        data = get_data.retrieve_game_data(game_id)
+
         clear_output(wait=True)
+        if data is None:
+            print(f"Game ID {game_id} doesn't exist")
+            clear_output(wait=True)
+            display(game_id_slider)
+            return
+        
         display(game_id_slider)  # Re-display the IntSlider widget
         print_game_info(data)
 
@@ -103,17 +117,65 @@ def display_season_data(season: str) -> None:
 
             display(game_id_slider)
             print_game_info(data)
-
             display(event_slider)
-
-            print("event id: ", event["eventId"])
-            print("event description: ", event["typeDescKey"])
-            if "details" in event:
-                if "xCoord" in event["details"] and "yCoord" in event["details"]:
-                    print("x: ", event["details"]["xCoord"], "y: ", event["details"]["yCoord"])
-                    plot_coords_on_image(event["details"]["xCoord"], event["details"]["yCoord"], debug_origin=False)
+            print_event_info(event)    
 
         event_slider.observe(on_event_id_change, names='value')
-
+    
+   
+    game_number = int(get_data.game_number_per_year[season])
+    game_id_slider = widgets.IntSlider(min=1, max=game_number, step=1, description='Game ID:')
 
     game_id_slider.observe(on_game_id_change, names='value')
+    display(game_id_slider)
+
+
+def print_playoff_game_info(game_id):
+    _, round, matchup, game = game_id.split('_')[2].split('-')
+    print(f"Round: {round}")
+    print(f"Matchup: {matchup}")
+    print(f"Game: {game}")
+    print()
+
+def display_playoff_season(season: str):
+
+    def on_game_id_change(change):
+        game_id = change['new']
+        clean_game_id = get_data.clean_playoff_game_id(game_id)
+        data = get_data.retrieve_game_data(clean_game_id)
+        clear_output(wait=True)
+
+        if data is None:
+            print(f"Game ID {game_id} doesn't exist")
+            display(team_dropdown)
+            return
+        
+        display(team_dropdown)  # Re-display the dropdown widget
+
+        print_game_info(data)
+        print_playoff_game_info(game_id)
+
+        events = data["plays"]
+        event_slider = widgets.IntSlider(min=0, max=len(events), step=1, description='Event no:') # event number is the index in the list of events
+        display(event_slider)
+
+        def on_event_id_change(change):
+            event = events[change['new']]
+            clear_output(wait=True)
+
+            display(team_dropdown)
+            print_game_info(data)
+            display(event_slider)
+            print_event_info(event)    
+
+        event_slider.observe(on_event_id_change, names='value')
+    
+    playoff_game_ids = get_data.playoff_game_id_generator(season)
+
+    team_dropdown = widgets.Dropdown(
+        options=playoff_game_ids,  # Example teams
+        value=None,
+        description='Game id:',
+    )
+    display(team_dropdown)
+    team_dropdown.observe(on_game_id_change, names='value')
