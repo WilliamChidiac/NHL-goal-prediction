b74420eba1a18d10defa68e63e163524b5edaa27
Andre Diler <andre.diler@polymtl.ca>
2024-10-07 11:12:01 -0400

    formatted code using black

--- ift6758/data/get_data.py
+++ ift6758/data/get_data.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import json
-from pathlib import Path 
+from pathlib import Path
 from json.decoder import JSONDecodeError
 
 # https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md
@@ -10,46 +10,49 @@ from json.decoder import JSONDecodeError
 REGULAR_SEASON = "02"
 PLAYOFFS = "03"
 
-'''
+"""
 1353 for seasons with 32 teams (2022 - Present)
 1271 for seasons with 31 teams (2017 - 2020)
 1230 for seasons with 30 team
 For playoff games, the 2nd digit of the specific number gives the round of the playoffs, the 3rd digit specifies the matchup, and the 4th digit specifies the game (out of 7).
-'''
+"""
 game_number_per_year = {
-    "2017": "1271", # 31 teams that play 82 games
+    "2017": "1271",  # 31 teams that play 82 games
     "2018": "1271",
-    "2019": "1082", # trial and error on the NHL API
-    "2020": "868", # trial and error on the NHL API
-    "2021": "1312", # 32 teams that play 82 games
+    "2019": "1082",  # trial and error on the NHL API
+    "2020": "868",  # trial and error on the NHL API
+    "2021": "1312",  # 32 teams that play 82 games
     "2022": "1312",
-    "2023": "1312"
-    }
+    "2023": "1312",
+}
+
 
 # will generate all playoff games, but some might not work
 # ex: if team wins 4 games in a row -> will advance to next game
 def playoff_game_id_generator(season) -> list[str]:
     game_ids = []
-    for round in range(1, 4+1):
+    for round in range(1, 4 + 1):
         if round == 1:
-            game_ids += matchups(season, round, 8) # 8 games in round 1 for 16 teams
+            game_ids += matchups(season, round, 8)  # 8 games in round 1 for 16 teams
         elif round == 2:
-            game_ids += matchups(season, round, 4) # 4 games in round 2 for 8 teams
+            game_ids += matchups(season, round, 4)  # 4 games in round 2 for 8 teams
         elif round == 3:
-            game_ids += matchups(season, round, 2) # 2 games in round 3 for 4 teams
+            game_ids += matchups(season, round, 2)  # 2 games in round 3 for 4 teams
         elif round == 4:
-            game_ids += matchups(season, round, 1) # 1 games in round 4 for 2 teams
+            game_ids += matchups(season, round, 1)  # 1 games in round 4 for 2 teams
     return game_ids
+
 
 def matchups(season, round, matchup_range):
     game_ids = []
-    for matchup in range(1, matchup_range +1):
-        for best_of_seven in range(1, 7+1):
+    for matchup in range(1, matchup_range + 1):
+        for best_of_seven in range(1, 7 + 1):
             game_ids.append(f"{season}_{PLAYOFFS}_0-{round}-{matchup}-{best_of_seven}")
     return game_ids
 
+
 def clean_playoff_game_id(game_id) -> str:
-    return game_id.replace('_', '').replace('-', '')
+    return game_id.replace("_", "").replace("-", "")
 
 
 def regular_season_game_id_generator(season) -> list[str]:
@@ -59,12 +62,15 @@ def regular_season_game_id_generator(season) -> list[str]:
         game_ids.append(game_id)
     return game_ids
 
+
 def download_data(game_id) -> dict:
-    response = requests.get(f"https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play")
+    response = requests.get(
+        f"https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play"
+    )
 
     if response.status_code == 200:
         data = response.json()
-        print("Downloaded game id: ", game_id)     
+        print("Downloaded game id: ", game_id)
         return data
     else:
         print(f"Failed to retrieve game ID {game_id}: {response.status_code}")
@@ -72,30 +78,35 @@ def download_data(game_id) -> dict:
 
 
 def load_cached_data(file_path) -> dict:
-     with open(file_path, 'r') as file:
-            print("using cached data for game id: ", file_path.split('/')[-1])  
-            try:
-                data = json.load(file)
-                return data
-            except JSONDecodeError:
-                print(f"Error decoding JSON from response for game ID: {file_path.split('/')[-1]}")
-            return None
-     
+    with open(file_path, "r") as file:
+        print("using cached data for game id: ", file_path.split("/")[-1])
+        try:
+            data = json.load(file)
+            return data
+        except JSONDecodeError:
+            print(
+                f"Error decoding JSON from response for game ID: {file_path.split('/')[-1]}"
+            )
+        return None
+
+
 def retrieve_game_data(game_id) -> dict:
-    data_dir = Path(__file__).resolve().parent.as_posix() + "/raw_data" # path to the raw data directory relative to this file
+    data_dir = (
+        Path(__file__).resolve().parent.as_posix() + "/raw_data"
+    )  # path to the raw data directory relative to this file
     filename = f"{game_id}.json"
     file_path = f"{data_dir}/{filename}"
-    
+
     if os.path.exists(file_path):
         data = load_cached_data(file_path)
         if data:
             return data
-        else: # file is present but corrupted: remove it, and redownload
+        else:  # file is present but corrupted: remove it, and redownload
             os.remove(file_path)
-  
+
     data = download_data(game_id)
-    if data: # do not create a file if the download failed    
-        with open(f"{data_dir}/{filename}", 'w') as file:
+    if data:  # do not create a file if the download failed
+        with open(f"{data_dir}/{filename}", "w") as file:
             json.dump(data, file)
-    
+
     return data
--- ift6758/visualizations/debugging_tool.py
+++ ift6758/visualizations/debugging_tool.py
@@ -1,4 +1,3 @@
-
 from PIL import Image
 from IPython.display import display, clear_output
 import matplotlib.pyplot as plt
@@ -7,16 +6,18 @@ import ipywidgets as widgets
 from ift6758.data import get_data
 
 # Load the image
-rink_image_path = '../figures/nhl_rink.png'
+rink_image_path = "../figures/nhl_rink.png"
 rink_image = Image.open(rink_image_path)
 
-    
-def plot_coords_on_image(rink_coord_x, rink_coord_y, image=rink_image, debug_origin=False) -> None:
-    
+
+def plot_coords_on_image(
+    rink_coord_x, rink_coord_y, image=rink_image, debug_origin=False
+) -> None:
+
     # Get dimensions
     image_length, image_height = image.size
-    rink_length=200
-    rink_height=85
+    rink_length = 200
+    rink_height = 85
 
     def feet_to_pixels(rink_coord_x, rink_coord_y):
 
@@ -39,16 +40,21 @@ def plot_coords_on_image(rink_coord_x, rink_coord_y, image=rink_image, debug_ori
         origin_x, origin_y = 0, 0
         adjusted_origin_x = origin_x + image_length / 2
         adjusted_origin_y = origin_y + image_height / 2
-        ax.plot(origin_x, origin_y, 'bo') 
-        ax.plot(adjusted_origin_x, adjusted_origin_y, 'go')
+        ax.plot(origin_x, origin_y, "bo")
+        ax.plot(adjusted_origin_x, adjusted_origin_y, "go")
 
     # Adjust coordinates to center origin
     adjusted_coords_x = image_coords_x + image_length / 2
     adjusted_coords_y = image_coords_y + image_height / 2
 
-
     # Plot the point with the adjusted coordinates
-    ax.plot(adjusted_coords_x, adjusted_coords_y, 'o', markersize=10, markerfacecolor='magenta')
+    ax.plot(
+        adjusted_coords_x,
+        adjusted_coords_y,
+        "o",
+        markersize=10,
+        markerfacecolor="magenta",
+    )
 
     # Generate axis labels
     step_size_x = rink_length / 8  # in feet
@@ -63,10 +69,10 @@ def plot_coords_on_image(rink_coord_x, rink_coord_y, image=rink_image, debug_ori
     ax.set_xticks(np.linspace(0, image_length, num=num_x))
     ax.set_yticks(np.linspace(0, image_height, num=num_y))
 
-    ax.set_xticklabels([f'{label:.1f}' for label in xtick_labels])
-    ax.set_yticklabels([f'{label:.1f}' for label in ytick_labels])
-    ax.set_xlabel('Rink Length (feet)')
-    ax.set_ylabel('Rink Height (feet)')
+    ax.set_xticklabels([f"{label:.1f}" for label in xtick_labels])
+    ax.set_yticklabels([f"{label:.1f}" for label in ytick_labels])
+    ax.set_xlabel("Rink Length (feet)")
+    ax.set_ylabel("Rink Height (feet)")
 
     # Invert the y-axis to match the image coordinate system
 
@@ -75,12 +81,13 @@ def plot_coords_on_image(rink_coord_x, rink_coord_y, image=rink_image, debug_ori
 
 def print_game_info(game_data):
     # TODO: display the sides of the team depending on the period (problem: HomeTeamDefendingSide available from 2019 only)
-    print("id" , game_data["id"])
-    print("date" , game_data["gameDate"])
+    print("id", game_data["id"])
+    print("date", game_data["gameDate"])
     print("Venue Location: ", game_data["venueLocation"]["default"])
     print("Home Team: ", game_data["homeTeam"]["name"]["default"])
     print("Away Team:", game_data["awayTeam"]["name"]["default"])
     print()
+
 
 def print_event_info(event):
     print("event id: ", event["eventId"])
@@ -88,9 +95,14 @@ def print_event_info(event):
     if "details" in event:
         if "xCoord" in event["details"] and "yCoord" in event["details"]:
             print("x: ", event["details"]["xCoord"], "y: ", event["details"]["yCoord"])
-            plot_coords_on_image(event["details"]["xCoord"], event["details"]["yCoord"], debug_origin=False)
+            plot_coords_on_image(
+                event["details"]["xCoord"],
+                event["details"]["yCoord"],
+                debug_origin=False,
+            )
         else:
             print("No coordinates available")
+
 
 def display_regular_season(season: str):
     def on_game_id_change(change):
@@ -103,44 +115,48 @@ def display_regular_season(season: str):
             clear_output(wait=True)
             display(game_id_slider)
             return
-        
+
         display(game_id_slider)  # Re-display the IntSlider widget
         print_game_info(data)
 
         events = data["plays"]
-        event_slider = widgets.IntSlider(min=0, max=len(events), step=1, description='Event no:') # event number is the index in the list of events
+        event_slider = widgets.IntSlider(
+            min=0, max=len(events), step=1, description="Event no:"
+        )  # event number is the index in the list of events
         display(event_slider)
 
         def on_event_id_change(change):
-            event = events[change['new']]
+            event = events[change["new"]]
             clear_output(wait=True)
 
             display(game_id_slider)
             print_game_info(data)
             display(event_slider)
-            print_event_info(event)    
+            print_event_info(event)
 
-        event_slider.observe(on_event_id_change, names='value')
-    
-   
+        event_slider.observe(on_event_id_change, names="value")
+
     game_number = int(get_data.game_number_per_year[season])
-    game_id_slider = widgets.IntSlider(min=1, max=game_number, step=1, description='Game ID:')
+    game_id_slider = widgets.IntSlider(
+        min=1, max=game_number, step=1, description="Game ID:"
+    )
 
-    game_id_slider.observe(on_game_id_change, names='value')
+    game_id_slider.observe(on_game_id_change, names="value")
     display(game_id_slider)
 
 
 def print_playoff_game_info(game_id):
-    _, round, matchup, game = game_id.split('_')[2].split('-')
+    _, round, matchup, game = game_id.split("_")[2].split("-")
     print(f"Round: {round}")
     print(f"Matchup: {matchup}")
     print(f"Game: {game}")
     print()
 
+
 def display_playoff_season(season: str):
 
     def on_game_id_change(change):
-        game_id = change['new']
+        game_id = change["new"]
         clean_game_id = get_data.clean_playoff_game_id(game_id)
         data = get_data.retrieve_game_data(clean_game_id)
         clear_output(wait=True)
@@ -149,33 +165,35 @@ def display_playoff_season(season: str):
             print(f"Game ID {game_id} doesn't exist")
             display(team_dropdown)
             return
-        
+
         display(team_dropdown)  # Re-display the dropdown widget
 
         print_game_info(data)
         print_playoff_game_info(game_id)
 
         events = data["plays"]
-        event_slider = widgets.IntSlider(min=0, max=len(events), step=1, description='Event no:') # event number is the index in the list of events
+        event_slider = widgets.IntSlider(
+            min=0, max=len(events), step=1, description="Event no:"
+        )  # event number is the index in the list of events
         display(event_slider)
 
         def on_event_id_change(change):
-            event = events[change['new']]
+            event = events[change["new"]]
             clear_output(wait=True)
 
             display(team_dropdown)
             print_game_info(data)
             display(event_slider)
-            print_event_info(event)    
+            print_event_info(event)
 
-        event_slider.observe(on_event_id_change, names='value')
-    
+        event_slider.observe(on_event_id_change, names="value")
+
     playoff_game_ids = get_data.playoff_game_id_generator(season)
 
     team_dropdown = widgets.Dropdown(
         options=playoff_game_ids,  # Example teams
         value=None,
-        description='Game id:',
+        description="Game id:",
     )
     display(team_dropdown)
-    team_dropdown.observe(on_game_id_change, names='value')
+    team_dropdown.observe(on_game_id_change, names="value")
