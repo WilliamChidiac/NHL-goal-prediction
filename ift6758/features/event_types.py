2715e469ae3f13321520f700599449e4c65d59b7
Andre Diler <andre.diler@polymtl.ca>
2024-10-08 19:05:50 -0400

    retrieved "shot-on-goal" events

--- ift6758/features/event_types.py
+++ ift6758/features/event_types.py
@@ -2,6 +2,7 @@ class PenaltyShotEvent:
     def __init__(
         self,
         game_id,
+        event_id,
         period,
         time,
         defending_player,
@@ -18,12 +19,14 @@ class PenaltyShotEvent:
         self.defending_player = defending_player  # commited the fault
         self.attacking_player = attacking_player  # who got the penalty shot
         self.owner_team = owner_team
+        self.event_id = event_id
 
 
 class ShotOnGoalEvent:
     def __init__(
         self,
         game_id,
+        event_id,
         period,
         time,
         shooting_player,
@@ -32,6 +35,7 @@ class ShotOnGoalEvent:
         y_coord,
         shot_type,
         owner_team,
+        zone_code,
     ):
         self.game_id = game_id
         self.x_coord = x_coord
@@ -42,12 +46,30 @@ class ShotOnGoalEvent:
         self.shot_type = shot_type
         self.goalie = goalie
         self.owner_team = owner_team
+        self.event_id = event_id
+        self.zone_code = zone_code
+
+    def to_dict(self):
+        return {
+            "game_id": self.game_id,
+            "period": self.period,
+            "time": self.time,
+            "shooting_player": self.shooting_player,
+            "x_coord": self.x_coord,
+            "y_coord": self.y_coord,
+            "owner_team": self.owner_team,
+            "goalie": self.goalie,
+            "shot_type": self.shot_type,
+            "zone_code": self.zone_code,
+            "event_id": self.event_id,
+        }
 
 
 class GoalEvent:
     def __init__(
         self,
         game_id,
+        event_id,
         period,
         time,
         scoring_player,
@@ -56,6 +78,7 @@ class GoalEvent:
         owner_team,
         goalie,
         shot_type,
+        zone_code,
     ):
         self.game_id = game_id
         self.x_coord = x_coord
@@ -66,6 +89,8 @@ class GoalEvent:
         self.owner_team = owner_team
         self.goalie = goalie
         self.shot_type = shot_type
+        self.event_id = event_id
+        self.zone_code = zone_code
 
     def to_dict(self):
         return {
@@ -78,4 +103,6 @@ class GoalEvent:
             "owner_team": self.owner_team,
             "goalie": self.goalie,
             "shot_type": self.shot_type,
+            "zone_code": self.zone_code,
+            "event_id": self.event_id,
         }
