3ae98d9e6042cdd72ac6fbb7b488d6eb849cb9f9
Andre Diler <andre.diler@polymtl.ca>
2024-11-16 11:41:47 -0500

    [refactor] refactor distance and angle calculation

---
+++ ift6758/features/trigonometry.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import math
+
+left_net_coords = (-100+11, 0)
+right_net_coords = (100-11, 0)
+
+def determine_enemy_net_coords(row):
+    event_owner_team = row['eventOwnerTeamId']
+    is_event_owner_home_team = row['homeTeam_id'] == event_owner_team
+    if is_event_owner_home_team: # pick goal at the opposite side of the owner team
+        net_coords = right_net_coords if row['homeTeamDefendingSide'] == "left" else left_net_coords
+    else:
+        net_coords = left_net_coords if row['homeTeamDefendingSide'] == "left" else right_net_coords
+    return net_coords
+
+def compute_distance_from_net(row):
+    net_coords = determine_enemy_net_coords(row)
+    distance = ((row['xCoord'] - net_coords[0])**2 + row['yCoord']**2)**0.5
+    return distance
+
+def compute_angle_from_net(row, positive_angle=True):
+    net_coords = determine_enemy_net_coords(row)
+    dx = row['xCoord'] - net_coords[0]
+    dy = row['yCoord'] - net_coords[1]
+    angle = math.atan2(dy, dx)  # Angle in radians
+    angle_degrees = math.degrees(angle)  # Convert to degrees
+    return abs(angle_degrees) if positive_angle else angle_degrees
+
+
+def plot_distances_histogram(df):
+    df['distance_from_net'].hist(bins=50)
+    plt.xlabel('Distance from Goal')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Distances from Goal')
+    plt.show()
